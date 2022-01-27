9aff2a9735f01a1faf8ddd315a065ecfa6a66331
GonzaloR98 <gonzaloriveravilches@gmail.com>
2022-01-27 15:39:03 +0100

    Manejando container

---
+++ manejando_container.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+module load singularity/3.8.0
+
+echo "La version del sistema operativo de la imagen es:" 
+echo
+singularity exec /nas/hdd-0/singularity_images/python_3-9-1.sif cat /etc/os-release
+
+echo "El contenido del directorio que estamos viendo es:"
+echo
+singularity exec /nas/hdd-0/singularity_images/python_3-9-1.sif ls -a
+
+echo "El path del directorio que estamos viendo:"
+echo
+singularity exec /nas/hdd-0/singularity_images/python_3-9-1.sif pwd
+
+echo "La version de python que está instalada es:"
+echo
+singularity exec /nas/hdd-0/singularity_images/python_3-9-1.sif python --version
+
+echo "La version del compilador de C++ que tenemos instalada es:"
+echo
+singularity exec /nas/hdd-0/singularity_images/python_3-9-1.sif g++ --version
+
+echo "Ejecutando el archivo k-mer13 para C++ y midiendo su tiempo de ejecucion:"
+echo
+time ./k-mer13
---
+++ output_manejando_container.sh
@@ -0,0 +1,49 @@
+La version del sistema operativo de la imagen es:
+
+PRETTY_NAME="Debian GNU/Linux 10 (buster)"
+NAME="Debian GNU/Linux"
+VERSION_ID="10"
+VERSION="10 (buster)"
+VERSION_CODENAME=buster
+ID=debian
+HOME_URL="https://www.debian.org/"
+SUPPORT_URL="https://www.debian.org/support"
+BUG_REPORT_URL="https://bugs.debian.org/"
+El contenido del directorio que estamos viendo es:
+
+.
+..
+.git
+README.md
+RESULTADOS.txt
+data
+k-mer13
+k-mer13.py
+k-mer14
+k-mer14.py
+manejando_container.sh
+output_manejando_container.sh
+singularity
+submit_eval_nativa.sh
+El path del directorio que estamos viendo:
+
+/home/alumno31/lab5
+La version de python que está instalada es:
+
+Python 3.9.1
+La version del compilador de C++ que tenemos instalada es:
+
+g++ (Debian 8.3.0-6) 8.3.0
+Copyright (C) 2018 Free Software Foundation, Inc.
+This is free software; see the source for copying conditions.  There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+Ejecutando el archivo k-mer13 para C++ y midiendo su tiempo de ejecucion:
+
+Start
+Number of generated k-mers: 67108864
+Finish!
+
+real    0m0,517s
+user    0m0,515s
+sys     0m0,001s
